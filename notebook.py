6c07d57d760dc7ac3b7a840f42011b2e34a9df22
Johan Ramirez <johand200505@gmail.com>
2026-07-14 14:47:17 -0400

    Changed ask_user to writing_entries

--- notebook.py
+++ notebook.py
@@ -2,10 +2,13 @@ class Notebook():
     def __init__(self, entries=0):
         self.entries = entries
 
-    def ask_user(self):
-        user_input = int(input("Enter the number of entries: "))
-        self.entries = user_input
-        return self.entries
+    def write_entries(self):
+        return "Writing entries"
 
-ask = Notebook(0)
-print(ask.ask_user())
+
+def main():
+    user_input = int(input("Enter the number of entries: "))
+    notes = Notebook(user_input)
+    print(notes.write_entries())
+
+main()
