80226c632252bc9b0b0bc71ee6b3192c9f65a37a
Johan Ramirez <johand200505@gmail.com>
2026-07-14 15:16:05 -0400

    Changed method writing_entries to create_entries; create_entries adds user input into a list and returns the list

--- notebook.py
+++ notebook.py
@@ -2,13 +2,20 @@ class Notebook():
     def __init__(self, entries=0):
         self.entries = entries
 
-    def write_entries(self):
-        return "Writing entries"
-
+    def create_entries(self):
+        """Writes entries for notebook"""
+        empty_list = []
+        for entry in range(1, self.entries + 1):
+            print(f"Write for entry #{entry}")
+            text = input("")
+            empty_list.append(text)
+            print(entry)
+        return empty_list
 
 def main():
     user_input = int(input("Enter the number of entries: "))
     notes = Notebook(user_input)
-    print(notes.write_entries())
+    print(notes.create_entries())
+
 
 main()
