1e2c14307096af6cf5843bf8352c5e0c281efbcf
Johan Ramirez <johand200505@gmail.com>
2026-07-15 13:22:34 -0400

    Created method add_more_entries, letting the user add more entries to their already created entries.

--- notebook.py
+++ notebook.py
@@ -12,14 +12,27 @@ class Notebook():
         return self.__list
     
     def view_entries(self):
-        pick = int(input(f"Choose entry(#1 - {len(self.__list)})"))
+        pick = int(input(f"Choose entry(#1 - {len(self.__list)}): "))
         view = self.__list[pick - 1]
         print(view)
+
+    def add_more_entries(self):
+        user_input = int(input("Enter the number of additional entries: "))
+        for entry in range(1, user_input + 1):
+            print(f"Write for entry #{len(self.__list) + 1}")
+            text = input("")
+            self.__list.append(text)
+        return self.__list
 
 def main():
     user_input = int(input("Enter the number of entries: "))
     notes = Notebook(user_input)
     notes.create_entries()
+
+    opt_user = input("Add more entries?(y/n): ")
+    notes.add_more_entries()
+    
+
     opt_user = input("View entries?(y/n): ")
     notes.view_entries()
 
