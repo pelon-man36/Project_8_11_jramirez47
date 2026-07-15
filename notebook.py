b3ebbff11f7d13fd6cf7d562c720cc0c0a8d3f90
Johan Ramirez <johand200505@gmail.com>
2026-07-15 13:08:46 -0400

    Created a method that lets the user view any chosen entry they created

--- notebook.py
+++ notebook.py
@@ -1,21 +1,27 @@
 class Notebook():
     def __init__(self, entries=0):
         self.entries = entries
+        self.__list = []
 
     def create_entries(self):
-        """Writes entries for notebook"""
-        empty_list = []
+        """Creates entries for a notebook"""
         for entry in range(1, self.entries + 1):
             print(f"Write for entry #{entry}")
             text = input("")
-            empty_list.append(text)
-            print(entry)
-        return empty_list
+            self.__list.append(text)
+        return self.__list
+    
+    def view_entries(self):
+        pick = int(input(f"Choose entry(#1 - {len(self.__list)})"))
+        view = self.__list[pick - 1]
+        print(view)
 
 def main():
     user_input = int(input("Enter the number of entries: "))
     notes = Notebook(user_input)
-    print(notes.create_entries())
+    notes.create_entries()
+    opt_user = input("View entries?(y/n): ")
+    notes.view_entries()
 
 
 main()
