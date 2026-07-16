c2fe9fcfb3c5c4f756be8d4b6c3caa987dd04d86
Johan Ramirez <johand200505@gmail.com>
2026-07-16 14:30:55 -0400

    Added print statements to make what is printed more cleaner.

--- notebook.py
+++ notebook.py
@@ -16,24 +16,43 @@ class Notebook():
         """Creates entries for a notebook"""
         for entry in range(1, self.entries + 1):
             print(f"Write for entry #{entry}")
+            print()
             text = input("")
+            print()
             self.__list.append(text)
         return self.__list
     
     def view_entries(self):
         """View created entries"""
-        pick = int(input(f"Choose entry(#1 - {len(self.__list)}): "))
-        view = self.__list[pick - 1]
-        print(view)
+        print()
+        try:
+            pick = int(input(f"Choose entry(#1 - {len(self.__list)}): "))
+        except ValueError:
+            print()
+            print("Invalid Option")
+            print()
+        else:
+            view = self.__list[pick - 1]
+            print()
+            print(view)
 
     def add_more_entries(self):
         """Adds more entries"""
-        user_input = int(input("Enter the number of additional entries: "))
-        for entry in range(1, user_input + 1):
-            print(f"Write for entry #{len(self.__list) + 1}")
-            text = input("")
-            self.__list.append(text)
-        return self.__list
+        try:
+            print()
+            user_input = int(input("Enter the number of additional entries: "))
+        except ValueError:
+            print()
+            print("Invalid Option")
+        else:
+            print()
+            for entry in range(1, user_input + 1):
+                print(f"Write for entry #{len(self.__list) + 1}")
+                print()
+                text = input("")
+                print()
+                self.__list.append(text)
+            return self.__list
     
     def store_entries(self):
         """Stores created entries in a json file (entries.json)"""
@@ -58,38 +77,53 @@ class Notebook():
 
 def main():
     notes = Notebook()
+    print()
+    print("---Notebook---")
+    print()
+
     if notes.file_exist():
         cont = True
         while cont:
             user_input = input("A file was found! Open these entries?(y/n) ")
+            print()
             if user_input == "y":
                 cont = False
                 notes.read_stored_entries()
                 notes.view_entries()
                 while True:
+                    print()
                     user_input = input("View another?(y/n) ")
                     if user_input == "y":
                         notes.view_entries()
                         continue
                     elif user_input == "n":
+                        print()
                         break
                     else:
+                        print()
                         print("Invalid Option")
+                        print()
             elif user_input == "n":
                 cont = False
             else:
                 print("Invalid Option")
+                print()
     cont = True
     while cont:
         user_input = input("Create entries?(y/n) ")
+        print()
         if user_input == "y":
             try:
                 user_input = int(input("Enter the number of entries: "))
+                print()
             except ValueError:
+                print()
                 print("Invalid Option")
+                print()
             else:
                 if user_input <= 0:
                     print("Invalid Option")
+                    print()
                 else:
                     notes = Notebook(user_input)
                     notes.create_entries()
@@ -100,35 +134,45 @@ def main():
                         elif opt_user == "n":
                             cont = False
                         else:
+                            print()
                             print("Invalid Option")
                     cont = True
                     while cont:
+                        print()
                         opt_user = input("View entries?(y/n): ")
                         if opt_user == "y":
                             notes.view_entries()
                         elif opt_user == "n":
+                            print()
                             cont = False
                         else:
+                            print()
                             print("Invalid Option")
 
                     cont = True
                     while cont:
                         opt_user = input("Store entries?(y/n) ")
                         if opt_user == "y":
+                            print()
                             notes.store_entries()
+                            print("Entries have been successfully stored!")
                             cont = False
                         elif opt_user == "n":
                             cont = False
                         else:
+                            print()
                             print("Invalid Option")
+                            print()
 
                     break
         elif user_input == "n":
             cont = False
         else:
             print("Invalid Option")
+            print()
 
-    print("Goodbye.")
+    print()
+    print("---End of Program---")
 
 
 
